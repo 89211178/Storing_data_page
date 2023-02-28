01577becf48f3fc55cb3a3c9c086d1259e3f2bf7
Lea Pajnic <lea.pajnic@gmail.com>
2023-02-28 18:51:50 +0100

    added conn and web

---
+++ back-end/index.js
@@ -0,0 +1,21 @@
+const express = require('express')
+const app = express()
+const dotenv = require("dotenv")
+dotenv.config()
+
+const port = process.env.PORT || 5012
+//Import opur custom modules-controllers
+const web= require("./routes/web")
+const db= require("./db/conn")
+
+app.get("/",(req,res)=>{
+res.send("Server is finding port ...")
+})
+
+//Routes
+app.use('/web', web);
+
+///App listening on port
+app.listen(process.env.PORT || port, ()=>{
+console.log(`Server is running on port: ${process.env.PORT || port}`)
+})
