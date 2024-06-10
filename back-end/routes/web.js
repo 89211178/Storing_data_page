828f1d8431770402ba7e1f6227de29b95a82a710
Lea <lea.pajnic@gmail.com>
2024-06-10 08:36:08 +0200

    first commit

--- back-end/routes/web.js
+++
@@ -1,9 +0,0 @@
-const express= require("express")
-const web = express.Router();
-
-web.get('/',(req,res)=>{
-    console.log("The route has been reached")
-    res.send("web")
-})
-
-module.exports=web
